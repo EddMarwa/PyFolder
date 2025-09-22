b40f09288fc99ee7f9f2e65c2156272de5ad1394
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-22 17:21:29 +0300

    looping through letters

--- hello.py
+++ hello.py
@@ -1,32 +1,34 @@
-print("hello, Python! Today is 22/09/2025.")
+#print("hello, Python! Today is 22/09/2025.")
 
-x, y, z = ["orange", "banana", "avocado"]
-print(x,y,z)
+#x, y, z = ["orange", "banana", "avocado"]
+#print(x,y,z)
 
-x = "python "
-y = "is "
-z = "awesome"
+#x = "python "
+#y = "is "
+#z = "awesome"
 
-print(x + y + z)
+#print(x + y + z)
 
-X = 5
-y = 10
-print(X + y)
-
-
+#X = 5
+#y = 10
+#print(X + y)
 
 
-x = "Eddy"
+#x = "Eddy"
 
-def surname():
-    x = "Chua"
-    print("My surname is " + x)
-surname()
-print("My first name is " + x)
+#def surname():
+ #   x = "Chua"
+ #   print("My surname is " + x)
+#surname()
+#print("My first name is " + x)
 
-name = "Edd" 
-age = 10 
-language = "python" 
+#name = "Edd" 
+#age = 10 
+#print(type(age))
+#language = "python" 
 #print ("My name is " + name + ", I am " + str(age) + " years old, and I am learning " + language)
 
-print(f"My name is {name}, I am {age} years old, and I am learning {language}.")
+#print(f"My name is {name}, I am {age} years old, and I am learning {language}.")
+
+for s in "banana":
+    print(s)
