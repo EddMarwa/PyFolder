c9ed9824ccc4dfc4609673337c4658a4db379d94
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-22 11:46:18 +0300

    update

--- hello.py
+++ hello.py
@@ -2,3 +2,13 @@ print("hello, Python! Today is 22/09/2025.")
 
 x, y, z = ["orange", "banana", "avocado"]
 print(x,y,z)
+
+x = "python "
+y = "is "
+z = "awesome"
+
+print(x + y + z)
+
+X = 5
+y = 10
+print(X + y)
