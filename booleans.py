c99e70e8db9842f154f89a5f60c7c8011df72026
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-23 07:50:42 +0300

    true

--- booleans.py
+++ booleans.py
@@ -1,4 +1,5 @@
 #23/09/2025 Starting from booleans.py
-print(True and False)  # Output: False
-print(True or False)
-print(not False)
+#print(True and False)  # Output: False
+#print(True or False)
+#print(not False)
+
