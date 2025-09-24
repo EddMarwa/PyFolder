546ab51d71a2dd47f0d7ecdb818aee45b754e5ac
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-24 07:09:58 +0300

    udt

--- Tuples.py
+++ Tuples.py
@@ -10,5 +10,5 @@
 
 # Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
 
-x = ("merceded", "ferrari", "Mclaren")
+x = ("mercedes", "ferrari", "Mclaren")
 print(x)
