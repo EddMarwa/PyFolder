39047ec84eacae5abe3364313d431f7a1a593be2
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-24 07:08:00 +0300

    cmt

--- Tuples.py
+++ Tuples.py
@@ -1,11 +1,11 @@
-thistuple = ("apple", "banana", "cherry")
-print(thistuple[1])
+#thistuple = ("apple", "banana", "cherry")
+#print(thistuple[1])
 
-thistuple = ("apple", "banana", "cherry")
-print(thistuple[-1])
+#thistuple = ("apple", "banana", "cherry")
+#print(thistuple[-1])
 
-thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
+#thistuple = ("apple", "banana", "cherry")
+#if "apple" in thistuple:
+ # print("Yes, 'apple' is in the fruits tuple")
 
 # Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
