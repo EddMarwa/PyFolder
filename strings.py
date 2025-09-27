c7583b7d0b4b407592357ea68737a75a11835277
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-27 20:21:29 +0300

    update

--- strings.py
+++ strings.py
@@ -7,3 +7,7 @@
 #price = 49
 #myorder = "I want {1} pieces of item number {0} for {2:.2f} dollars."
 #print(myorder.format(quantity, itemno, price))
+
+print("Enter your name: ")
+name = input()
+print(f"Hello, {name} !" )
