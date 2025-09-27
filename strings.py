db6a4b8b66d5deda24b7448b150573c6ab77cae0
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-27 20:17:38 +0300

    udt

--- strings.py
+++ strings.py
@@ -1,3 +1,9 @@
-price = 5
-meso = f"this is a prompt and the price is {price:.1f} shillings"
-print(meso)
+#price = 55464
+#meso = f"this is a prompt and the price is {price:,} shillings"
+#print(meso)
+
+#quantity = 3
+#itemno = 567
+#price = 49
+#myorder = "I want {1} pieces of item number {0} for {2:.2f} dollars."
+#print(myorder.format(quantity, itemno, price))
