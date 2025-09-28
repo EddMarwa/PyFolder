730a6297fa76a8abadecda5be2d9667ef0fbd6ad
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-29 01:52:31 +0300

    dict extradiction

--- dict.py
+++ dict.py
@@ -1,3 +1,10 @@
 colors = {
-    "red": "number 44"
+    "red": "number 44",
+    "blue": "number 1",
+    "green": "number 5"
 }
+
+print(colors)
+print(type(colors))
+print(len(colors))
+print(colors["blue"])
