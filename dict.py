dfb0265aee1f34bd0f7bb94244a76304192d75d4
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-29 15:52:25 +0300

    cmt out

--- dict.py
+++ dict.py
@@ -16,15 +16,15 @@
 #3print(colors["blue"])
 
 
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-x = car.keys()
-print(x) #before the change
-car["color"] = "white"
-print(x) #after the change
+#car = {
+#"brand": "Ford",
+#"model": "Mustang",
+#"year": 1964
+#}
+#x = car.keys()
+#print(x) #before the change
+#car["color"] = "white"
+#print(x) #after the change
 
 
 myfamily = {
