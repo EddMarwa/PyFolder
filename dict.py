016bcadc843775128af7013a8b2e5957ad0815c6
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-29 14:28:06 +0300

    pod

--- dict.py
+++ dict.py
@@ -9,8 +9,8 @@
 #print(len(colors))
 #print(colors["blue"])
 
-colors = {"red": "number 44", "blue": "number 1", "green": "number 5"}
-print(colors)
-print(type(colors))
-print(len(colors))
-print(colors["blue"])
+#colors = {"red": "number 44", "blue": "number 1", "green": "number 5"}
+#print(colors)
+#print(type(colors))
+#print(len(colors))
+#3print(colors["blue"])
