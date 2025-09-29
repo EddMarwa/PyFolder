8bb79544574827132dca5ee1c6cd54cc735ec04d
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-29 14:29:52 +0300

    dict

--- dict.py
+++ dict.py
@@ -14,3 +14,14 @@
 #print(type(colors))
 #print(len(colors))
 #3print(colors["blue"])
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x) #before the change
+car["color"] = "white"
+print(x) #after the change
