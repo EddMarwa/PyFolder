7852e3ab4907ab17178a84a4499cc20b24f0d42c
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-30 23:18:51 +0300

    nv r

--- While loop.py
+++ While loop.py
@@ -1,10 +1,16 @@
 # While loop
 # for loop
 
+#i = 1
+#while i < 6:
+  #i += 1
+  #print(i)
+  #if not (i == 3):
+   # break
+ 
 i = 1
 while i < 6:
-  i += 1
   print(i)
-  if not (i == 3):
-    break
- 
+  i += 1
+else:
+  print("i is no longer less than 6")
