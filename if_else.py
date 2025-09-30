8663389eb870f0d39840a31be89e0a094a16c2dd
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-30 22:49:07 +0300

    cmt out

--- if_else.py
+++ if_else.py
@@ -6,11 +6,11 @@
 #  print("a and b are equal")
 
 
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  pass
+#a = 200
+#b = 33
+#if b > a:
+#  print("b is greater than a")
+#elif a == b:
+#  print("a and b are equal")
+#else:
+ # pass
