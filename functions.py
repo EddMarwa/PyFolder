95fa99cd1a97409732458abfb42e6bfa4d3d0e60
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-01 22:01:49 +0300

    tri_recursion

--- functions.py
+++ functions.py
@@ -21,3 +21,14 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 
 my_function(fruits)
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
