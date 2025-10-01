de1d14ebc7909a1729a12bea51d3da8cd03b2aa3
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-01 23:01:22 +0300

    lambda

--- aob in py.py
+++ aob in py.py
@@ -1,6 +1,10 @@
 # Demonstrating the use of 'in' with a range object
-r = range(0, 10, 2)
-print(list(r))
-print(6 in r)
-print(7 in r)
+#r = range(0, 10, 2)
+#print(list(r))
+#print(6 in r)
+#print(7 in r)
+#
+for i in range(3):
+    print(i)
 
+#python lambda.py
