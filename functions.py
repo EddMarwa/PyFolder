b64e22648bee0350fa65a2d8f221f27b86ec1cca
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-01 21:38:36 +0300

    dts

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 #functions 
-def fn():
-    print("Hello from functions")
-fn()
+#def fn():
+#    print("Hello from functions")
+#fn()
+
+def fn2(fname, lname):
+    print(fname + " " + lname)
+
+fn2 = ("Edd", "Mar")
