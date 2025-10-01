fdfcf59d401c7690d71966b988b9d95409407a0e
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-01 21:49:51 +0300

    defining a blank fun

--- functions.py
+++ functions.py
@@ -3,6 +3,14 @@
 #    print("Hello from functions")
 #fn()
 
-def fn2(fname, lname):
-    print(fname + " " + lname)
-fn2 ("Edd", "Mar")
+#def fn2(fname, lname):
+#    print(fname + " " + lname)
+#fn2 ("Edd", "Mar")
+
+#def fn3(country = "Kenya"):
+ #   print("I am from " + country)
+
+#fn3("tanzania")
+#fn3("uganda")
+#fn3()
+#fn3("rwanda")
