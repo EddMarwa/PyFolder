4d533d1df3fae8d8cd1c24798feec72156564511
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-05 21:21:35 +0300

    ops

---
+++ Mini-Projects/NumGuess.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1,100)
+guess = 0
+
+while guess != number:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You've guessed the number.")
