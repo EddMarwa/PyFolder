c945032dd6ade3090417115cc7c284cdda1677f0
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-10-20 07:50:46 +0300

    update

--- basics/Json.py
+++ basics/Json.py
@@ -1,12 +1,14 @@
 
-import json
+#import json
 
 # some JSON:
-x = '{ "name":"John", "age":30, "city":"New York"}'
+#x = '{ "name":"John", "age":30, "city":"New York"}'
 
 # parse x:
-y = json.loads(x)
+#y = json.loads(x)
 
 # the result is a Python dictionary:
-print(y["city"])
+#print(y["city"])
+
+
 
